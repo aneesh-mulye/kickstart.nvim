e8420c5e41dad7c13f314a116766e4ca0e541cff
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2025-01-09 03:50:38 +0530

    Initial config, and add Clojure LSP

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Conjure
+  {
+    'Olical/conjure',
+    ft = { 'clojure', 'fennel', 'python' }, -- etc
+    lazy = true,
+    init = function()
+      -- Set configuration options here
+      -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+      -- This is VERY helpful when reporting an issue with the project
+      vim.g['conjure#debug'] = true
+    end,
+  },
+}
